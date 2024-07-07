8f33e44cb48a5ccad60c89ea78bb396aabad37ec
max_on <maks_on_max@mail.ru>
2024-07-07 20:09:02 +0700

    Добавьте файлы проекта.

---
+++ VolumeReader/Program.cs
@@ -0,0 +1,29 @@
+﻿using VolumeReader;
+
+internal class Program
+{
+  private static bool p_isCorrect;
+  private static void Main(string[] args)
+  {
+    while (!p_isCorrect)
+    {
+      Console.Write($"Введите URL папки: ");
+
+      string? path = Console.ReadLine();
+
+      if (string.IsNullOrWhiteSpace(path))
+      {
+        Console.WriteLine($"URL некорректный");
+      }
+      else if (!Directory.Exists(path))
+      {
+        Console.WriteLine($"Указанного каталога не существует!");
+      }
+      else
+      {
+        Console.WriteLine(Reader.GetDirVolume(path, 0));
+        p_isCorrect = true;
+      }
+    }
+  }
+}
---
+++ VolumeReader/Reader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeReader
+{
+  internal class Reader
+  {
+    public static long GetDirVolume(string _url, long _volume)
+    {
+      try
+      {
+        var dirInfo = new DirectoryInfo(_url);
+
+        var files = dirInfo.GetFiles();
+
+        if(files != null)
+        {
+          foreach(var file in files)
+          {
+            _volume += file.Length;
+          }
+          var dirs = dirInfo.GetDirectories();
+
+          if (dirs != null)
+          {
+            foreach (var dir in dirs)
+            {
+              _volume = GetDirVolume(dir.FullName, _volume);
+            }
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+      return _volume;
+    }
+  }
+}
